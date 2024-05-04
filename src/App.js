f32196cd7aa1c0e673a71143510fab73c94e842f
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-05-04 12:33:59 +0530

    add the .env argument segment

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import EditorPage from "./pages/EditorPage";
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import Home from './pages/Home';
+import EditorPage from './pages/EditorPage';
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/editor/:roomId" element={<EditorPage />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+    return (
+        <>
+            <div>
+                <Toaster
+                    position="top-right"
+                    toastOptions={{
+                        success: {
+                            theme: {
+                                primary: '#4aed88',
+                            },
+                        },
+                    }}
+                ></Toaster>
+            </div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />}></Route>
+                    <Route
+                        path="/editor/:roomId"
+                        element={<EditorPage />}
+                    ></Route>
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
